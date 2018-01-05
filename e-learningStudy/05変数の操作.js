8d7df59a017bcbceac521aad3ddff387fbbd0d99
f.sakurai <f.sakurai@cci.co.jp>
2018-01-05 15:26:04 +0900

    javascript学習

---
+++ e-learningStudy/05変数の操作.js
@@ -0,0 +1,12 @@
+//  javascript入門
+// 05:変数の操作
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var player = '戦士';
+console.log(player);
+
+console.log(player + "は、荒野を歩いていた");
+console.log(player + "は、モンスターと戦った");
+console.log(player + "は、モンスターをたおした");
